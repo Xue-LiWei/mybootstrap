69100507104ae161e0a7487da0d680cda5b8e90e
xueliwei <1241207923@qq.com>
2020-07-12 03:35:24 +0800

    第二次提交（更新）

---
+++ js/index.js
@@ -0,0 +1,88 @@
+// // features图片切换
+var i=0;
+var timer;
+function go() {
+    timer=setInterval(function () {
+        i++;
+        if(i>6){
+            i=0;
+            $('.change-box li').css({'top':'0px'});
+        }
+        for(var j=0;j<6;j++){
+            $('.change-box li').eq(j).css({'z-index':10-j});
+        }
+        // $('.change-box>ul').css({'top':-600*i+'px'});
+        // $('.change-box li').eq(i).addClass('addchange').siblings('.change-box li').removeClass('addchange');
+
+        if(i>0){
+            $('.change-box li').eq(i-1).animate({'top':600+'px'},2000);
+        }else{
+            $('.change-box li').eq(i).css({'top':'0px'});
+        }
+        if(i<6){
+            $('.features-btn').eq(i).addClass('addfbtn').siblings('.features-btn').removeClass('addfbtn');
+            if(i==3){$('.features-btn').eq(2).removeClass('addfbtn');}
+            if(i==0){$('.features-btn').eq(5).removeClass('addfbtn');}
+        }else{
+            $('.features-btn').eq(0).addClass('addfbtn').siblings('.features-btn').removeClass('addfbtn');
+            if(i==6){$('.features-btn').eq(5).removeClass('addfbtn');}
+        }
+        // console.log(i);
+    },3500);
+}
+
+go();
+
+$('.btn-f').click(function () {
+    clearInterval(timer);
+    for(var j=0;j<6;j++){
+        $('.change-box li').eq(j).css({'z-index':10-j});
+    }
+    $(this).siblings().removeClass('addfbtn');
+    if($(this).index()==0){
+        $('.change-box li').eq(0).stop().animate({'top':'0px'},1000);
+    }
+    for (var h=1;h<=$(this).index();h++){
+        $('.change-box li').eq(h).stop().animate({'top':'0px'},1000);
+        if($(this).index()>0){
+            $('.change-box li').eq($(this).index()-h).stop().animate({'top':600+'px'},1000);
+            $('.change-box li').eq(6).css({'top':600+'px'});
+        }
+    }
+    i=$(this).index();
+    setTimeout(function () {
+        clearInterval(timer);
+        go();
+        $(this).removeClass('addfbtn');
+    },3500);
+})
+
+$('.btn-r').click(function () {
+    clearInterval(timer);
+    $('.btn-l').eq(0).removeClass('addfbtn');
+    for(var j=0;j<6;j++){
+        $('.change-box li').eq(j).css({'z-index':10-j});
+    }
+    $(this).siblings().removeClass('addfbtn');
+    for (var h=1;h<=$(this).index()+3;h++){
+        if($(this).index()==2){
+            $('.change-box li').eq(4).stop().animate({'top':'600px'},1000);
+        }
+        $('.change-box li').eq(h+2).stop().animate({'top':'0px'},1000);
+        $('.change-box li').eq($(this).index()+3-h).stop().animate({'top':600+'px'},1000);
+        $('.change-box li').eq(6).css({'top':'0px'});
+    }
+    i=$(this).index()+3;
+    setTimeout(function () {
+        clearInterval(timer);
+        go();
+        $(this).removeClass('addfbtn');
+    },3500);
+})
+
+
+
+
+
+
+
